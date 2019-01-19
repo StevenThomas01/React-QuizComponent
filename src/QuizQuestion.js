6effc835c7235f31caaca106cc59a8a65f906163
ST <kietlo@gmail.com>
2019-01-19 23:40:52 +0000

    Add event handlers

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
-import QuizQuestionButton from "./QuizQuestionButton.js"
+import QuizQuestionButton from "./QuizQuestionButton.js";
 
 class QuizQuestion extends Component {
+  handleClick(button_text) {
+    if (button_text === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
+  }
+
   render() {
     return (
       <main>
@@ -10,7 +16,17 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    key={index}
+                    button_text={answer_option}
+                    clickHandler={this.handleClick.bind(this)}
+                  />
+                );
+              }
+            )}
           </ul>
         </section>
       </main>
